5332c189d4607f4640bb52787eabc18353e29ed8
nonahgame <103342324+nonahgame@users.noreply.github.com>
2025-09-05 18:44:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -483,7 +483,7 @@ def ai_decision(df, stop_loss_percent=STOP_LOSS_PERCENT, take_profit_percent=TAK
         elif close_price >= take_profit:
             logger.info("Take-profit triggered.")
             action = "sell"
-        elif (kdj_j > kdj_d and kdj_j > 65.00 and ema1 > ema2 rsi > 60.00): # and macd_hist > 1.00
+        elif (kdj_j > kdj_d and kdj_j > 65.00 and ema1 > ema2 and rsi > 60.00): # and macd_hist > 1.00
             logger.info(
             f"Sell triggered: kdj_j={kdj_j:.2f}, kdj_d={kdj_d:.2f}, "
             f"macd_hist={(macd - macd_signal):.2f}, close={close_price:.2f}"
